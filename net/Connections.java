3bb997a9f196a55baec9c7ef62307ace34f22e6b
geronimoooooooo <geronimoooooooo@gmail.com>
2015-06-26 14:43:51 +0200

    Create Connections.java

---
+++ net/Connections.java
@@ -0,0 +1,6 @@
+Difference between URLConnection, HttpURLConnection and HttpsURLConnection
+http://stackoverflow.com/questions/3920419/difference-between-urlconnection-httpurlconnection-and-httpsurlconnection
+
+
+http://stackoverflow.com/questions/1485708/how-do-i-do-a-http-get-in-java
+http://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
