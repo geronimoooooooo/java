51e965d3a97990ad5c21d29848cb5c8312c6066b
geronimoooooooo <geronimoooooooo@gmail.com>
2015-07-02 16:14:21 +0200

    Update Streams.java

--- Streams.java
+++ Streams.java
@@ -1,2 +1,9 @@
 Hier findet man alle möglichen Stream Examples:
 https://docs.oracle.com/javase/tutorial/essential/io/bytestreams.html
+
+
+InputStream is = urlc.getInputStream();
+    int c;
+    while ((c = is.read()) != -1)
+      System.out.print((char) c);
+  }
