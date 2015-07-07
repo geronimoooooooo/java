b872135dfb8f0c7d1c3b0f83b9bcb44f2fea84ac
geronimoooooooo <geronimoooooooo@gmail.com>
2015-07-07 16:58:45 +0200

    Update json.java

--- json.java
+++ json.java
@@ -1,2 +1,3 @@
 http://stackoverflow.com/questions/16377754/parse-json-file-using-gson
 http://stackoverflow.com/questions/20152710/gson-get-json-value-from-string
+http://www.javacreed.com/simple-gson-example/
