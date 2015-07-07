298465701a406613b9c870fb20b7c1a212443d9d
geronimoooooooo <geronimoooooooo@gmail.com>
2015-07-07 14:56:32 +0200

    Update ReadWriteInServlets.java

--- readWrite/ReadWriteInServlets.java
+++ readWrite/ReadWriteInServlets.java
@@ -1,3 +1,22 @@
+http://stackoverflow.com/questions/2308188/getresourceasstream-vs-fileinputstream
+
+getResourceAsStream is the right way to do it for web apps (as you already learned).
+getResourceAsStream() will load a file path relative from your application's classpath.
+
+The reason is that reading from the file system cannot work if you package your web app in a WAR. 
+This is the proper way to package a web app. 
+It's portable that way, because you aren't dependent on an absolute file path or 
+the location where your app server is installed.
+You don't want to hardcode or guess the absolute path in Java (web)applications. 
+That's only portability trouble (i.e. it runs in system X, but not in system Y). 
+The normal practice is to place those kind of resources in the classpath, 
+or to add its full path to the classpath (in an IDE like Eclipse that's the src folder and
+the "build path" respectively). This way you can grab them with help of the 
+ClassLoader by  ClassLoader#getResource() or ClassLoader#getResourceAsStream(). 
+It is able to locate files relative to the "root" of the classpath, as you by coincidence figured out. 
+In webapplications (or any other application which uses multiple classloaders) it's recommend 
+to use the ClassLoader as returned by Thread.currentThread().getContextClassLoader() for this.
+-------------------------------------------------------
 Bei Servlets: css2.css in den ordner src/resources abstellen
 try 
 (BufferedReader br = new BufferedReader(new InputStreamReader(
