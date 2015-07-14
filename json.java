156b07a5887a4692742ee632b12aa561f822d042
geronimoooooooo <geronimoooooooo@gmail.com>
2015-07-14 17:11:26 +0200

    Update json.java

--- json.java
+++ json.java
@@ -2,7 +2,8 @@ http://stackoverflow.com/questions/16377754/parse-json-file-using-gson
 http://stackoverflow.com/questions/20152710/gson-get-json-value-from-string
 http://www.javacreed.com/simple-gson-example/
 
-JsonObject jobj = new Gson().fromJson(jsonText, JsonObject.class);
+Gson gson = new Gson();
+JsonObject jobj = gson.fromJson(jsonText, JsonObject.class);
 String description_Value=jobj.get("content").getAsJsonObject().get("description").getAsString();
 
 {
