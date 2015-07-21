ae556a593d04e8ad6e8d3f0be22d4914a81f6b9f
geronimoooooooo <geronimoooooooo@gmail.com>
2015-07-21 12:12:23 +0200

    Update hashmap.java

--- hashmap.java
+++ hashmap.java
@@ -1,3 +1,7 @@
 for (String s : ts.hash_parameter.keySet()) {
-				System.out.println(s + " : " + ts.hash_parameter.get(s));
-			}
+		System.out.println(s + " : " + ts.hash_parameter.get(s));
+	}
+---------------------------------------------------------------------------------
+10 Examples:
+http://java67.blogspot.co.at/2013/02/10-examples-of-hashmap-in-java-programming-tutorial.html
+---------------------------------------------------------------------------------
