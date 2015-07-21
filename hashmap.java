d6c618e6418fe303f588063bc6e86df758070f7d
geronimoooooooo <geronimoooooooo@gmail.com>
2015-07-21 12:46:53 +0200

    Update hashmap.java

--- hashmap.java
+++ hashmap.java
@@ -1,3 +1,7 @@
+Iterate over Hashmap:
+http://www.mkyong.com/java/how-to-loop-a-map-in-java/
+http://java67.blogspot.co.at/2013/08/best-way-to-iterate-over-each-entry-in.html
+--------------------------------------------------------------------------------
 for (String s : ts.hash_parameter.keySet()) {
 		System.out.println(s + " : " + ts.hash_parameter.get(s));
 	}
