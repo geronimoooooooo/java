622582a3aafbe803b68fd5c0f77f7a311df930b1
geronimoooooooo <geronimoooooooo@gmail.com>
2015-07-22 18:21:15 +0200

    Update json.java

--- json.java
+++ json.java
@@ -2,16 +2,39 @@ http://stackoverflow.com/questions/16377754/parse-json-file-using-gson
 http://stackoverflow.com/questions/20152710/gson-get-json-value-from-string
 http://www.javacreed.com/simple-gson-example/
 
-Gson gson = new Gson();
-JsonObject jobj = gson.fromJson(jsonText, JsonObject.class);
-String description_Value=jobj.get("content").getAsJsonObject().get("description").getAsString();
+		Gson gson = new Gson();
+		JsonObject jobj = gson.fromJson(jsonText, JsonObject.class);
+					
+		String description_Value = jobj.get("content").getAsJsonObject().get("description").getAsString();
+		JsonArray jarr = jobj.get("content").getAsJsonObject().get("keywords").getAsJsonArray();
+				
+		ArrayList<String> list_keywords = new ArrayList<String>();
+		list_keywords = gson.fromJson(jarr.toString(), ArrayList.class);
+		
+		ObservedProperty [] obser =new Gson().fromJson(jobj.get("content").getAsJsonObject().get("observedProperties"), ObservedProperty[].class);
+		String xyz= jobj.get("content").getAsJsonObject().get("observedProperties").getAsJsonArray().get(0).getAsJsonObject().get("observedPropertyName").getAsString();
+		
+		ObservedProperty ob =new Gson().fromJson(jobj.get("content").getAsJsonObject().get("observedProperties").getAsJsonArray().get(1), ObservedProperty.class);
 
 {
 	"id": "Fellbachquelle",
 	"content":{
 		"description": "this is the description",
-		"key2": "value2",
-		"key3": "value3"
+		"keywords": ["weather","hydrology","gauge"],
+		"observedProperties":[
+			{			
+				"observedPropertyName" = "Temperatur",
+				"observedPropertyNameHref"="http://..",
+				"quality_text_value" = "here ist quality value",
+				"uomTitle" = "title1"
+			},
+			{
+				"observedPropertyName" = "Wasserpegel",
+				"observedPropertyNameHref"="http://..",
+				"quality_text_value" = "here ist quality value",
+				"uomTitle" = "title2"
+			}
+		]
 	}	
 }
 ----------------------------------------------------------------------------------------------
