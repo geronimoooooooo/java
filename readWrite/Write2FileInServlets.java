88fe9f752fd2b503420d603a901a0d3d51ff1658
geronimoooooooo <geronimoooooooo@gmail.com>
2015-07-23 17:07:32 +0200

    Create Write2FileInServlets.java

---
+++ readWrite/Write2FileInServlets.java
@@ -0,0 +1,4 @@
+ Files should generally be stored outside of the app directory.
+ Beim neuen Redeployen einer WAR File, würde der Inhalt des alten "xploded War files" 
+ dann mit all den abgespeicherten Textdateien überschrieben werden.
+ 
