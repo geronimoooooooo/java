331bf785f3ee6b1ba4d646ffacf4adc3e53921dc
geronimoooooooo <geronimoooooooo@gmail.com>
2015-08-14 15:05:54 +0200

    Create errors.java

---
+++ errors.java
@@ -0,0 +1,19 @@
+-------------------------------
+SOS
+-------------------------------
+Ein uom bei InsertSensor.xml wird vom SOS nicht akzeptiert.
+<ows:Exception exceptionCode="InvalidRequest" locator="string value 'cm ü.PNP' does not match pattern for UomSymbol in namespace http://www.opengis.net/swe/1.0.1">
+----------------------------------------
+
+XML
+-------------------------------------------
+sehr häufiger Fehler wenn man versucht einen String einzulesen, es wird aber eine URI bzw. InputStream erwartet
+Document document = dbuilder.parse(xml_org);
+
+java.net.MalformedURLException: no protocol:
+--------------------------------
+Wenn man versucht einen leeren xmlString zu  Document document = dbuilder.parse(xml_org);
+
+[Fatal Error] :1:1: Vorzeitiges Dateiende.
+org.xml.sax.SAXParseException; lineNumber: 1; columnNumber: 1; Vorzeitiges Dateiende.
+--------------------------------
