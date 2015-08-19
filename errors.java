dbd2ad68a50fa04b19dc626a060bbaa86dcf4133
geronimoooooooo <geronimoooooooo@gmail.com>
2015-08-19 13:09:53 +0200

    Update errors.java

--- errors.java
+++ errors.java
@@ -17,3 +17,7 @@ Wenn man versucht einen leeren xmlString zu  Document document = dbuilder.parse(
 [Fatal Error] :1:1: Vorzeitiges Dateiende.
 org.xml.sax.SAXParseException; lineNumber: 1; columnNumber: 1; Vorzeitiges Dateiende.
 --------------------------------
+Content is not allowed in Prolog.
+
+Einfach mal die ersten paar Zeichen löschen und neu schreiben. Dann sollte gehen.
+--------------------------------
