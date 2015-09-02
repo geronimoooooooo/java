d1f21910d4d4de748bced96046fa6e8b75ded8b6
geronimoooooooo <geronimoooooooo@gmail.com>
2015-09-02 13:18:09 +0200

    Create javadoc.java

---
+++ javadoc.java
@@ -0,0 +1,11 @@
+Bilder müssen .gif sein
+
+/**
+ * Used to read, write files, directories and work with strings!
+ * <p><IMG SRC="../doc-files/zr_sos_tstp.gif">
+ * @author slukic
+ */
+public class TextFiles {
+
+src/utility/TextFiles.java
+src/doc-files/zr_sos_tstp.gif
