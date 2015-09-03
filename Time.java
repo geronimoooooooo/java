9f8e44416a85d506e0a424a32fbf84543f839713
geronimoooooooo <geronimoooooooo@gmail.com>
2015-09-03 17:07:05 +0200

    Update Time.java

--- Time.java
+++ Time.java
@@ -45,3 +45,12 @@ public String getCurrentTimeStamp() {
     return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date());
 }
 ---------------------------------------------------------------
+DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+		Date date = new Date();
+		System.out.println(dateFormat.format(date)); //2015-08-14T11:22:46:131Z  use :SSS for ms
+		//time_value=dateFormat.format(date);
+---------------------------------------------------------------
+DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+		Date date = new Date();
+		System.out.println(dateFormat.format(date)); //2015-08-14T11:22:46:131Z  use :SSS for ms
+		//time_value=dateFormat.format(date);
