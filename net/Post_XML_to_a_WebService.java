e71631c3a01d83c908903e53d866f4a29717f139
geronimoooooooo <geronimoooooooo@gmail.com>
2015-10-29 14:45:26 +0100

    Update Post_XML_to_a_WebService.java

--- net/Post_XML_to_a_WebService.java
+++ net/Post_XML_to_a_WebService.java
@@ -1,61 +1,67 @@
 http://alexrogan.com/?p=41
 ein vollständiges code example
 -----------------------------------
-
-public void sendPOST2Webservice(String urlService, String msg){
-		msg= new ReaderWriter().readTextFileWithServletsAsStream("gor.xml");
-		System.out.println(msg);
-		//urlService="http://ispacevm30.researchstudio.at/sos41/service";
-		urlService="http://ispacevm30.researchstudio.at/focus/service"; 
+public String sendPOST2Webservice(String urlService, String msg){
+		//msg= TextFiles.readTextFileWithServletsAsStream_UTF8("InsertSensor.xml");
+		//System.out.println("msg2send: "+msg);
+			
+		//urlService="http://ispacevm30.researchstudio.at/focus/service"; 
 		String charset = "UTF-8";
 		OutputStreamWriter writer = null;
 		HttpURLConnection con = null;
+		String response_txt ="";
+		InputStream iss = null;
 		
 		try {
-	URL url = new URL(urlService);
-			con = (HttpURLConnection)url.openConnection();
-			con.setDoOutput(true); //triggers POST; sets implicitly to POST
-			con.setDoInput(true);
-			//con.setRequestMethod("POST");
-			con.setRequestProperty("accept-charset", charset);
-			con.setRequestProperty("Content-Type", "application/soap+xml");
-			//("Content-Type", "application/x-www-form-urlencoded;charset=" + charset);
+			//byte [] bray = msg.getBytes("UTF-8");
+			//msg = new String(bray);
 			
-						
-			writer = new OutputStreamWriter(con.getOutputStream());
-			writer.write(msg); //send POST data string
+			URL url = new URL(urlService);
+			con = (HttpURLConnection)url.openConnection();
+			con.setDoOutput(true); //triggers POST
+			con.setDoInput(true);
+			con.setRequestMethod("POST");
+			con.setRequestProperty("accept-charset", charset);
+			//con.setRequestProperty("Content-Type", "application/soap+xml");
+			con.setRequestProperty("Content-Type", "application/soap+xml;charset=UTF-8");
+			
+			//
+			writer = new OutputStreamWriter(con.getOutputStream(), charset);
+			writer.write(msg); //send POST data string		
+			//System.out.println("this is sendPOST2Webservice: "+msg);
 			writer.flush();
 			writer.close();
-			
+			Boolean error = false;
 			BufferedReader br = null;
-			int status = con.getResponseCode();
-			//https://de.wikipedia.org/wiki/HTTP-Statuscode
-			if(status>=200 && status<204){
+			
+			if(con.getResponseCode()==200){
 			//get response from the web service
 			//InputStream is = con.getInputStream();
-				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-				//used to see the serverside specified content-type
-				//String contentType = connection.getHeaderField("Content-Type");
+				
+				//macht aus &%/ Fehlern ein ganz normales ü
+				br = new BufferedReader(new InputStreamReader(con.getInputStream(),charset));
 			}else{
-				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream(),charset));
+				error = true;
 			}
-			String response_txt ="";
+			
 			String line ="";
 			StringBuilder strBuilder = new StringBuilder();
-				
-			System.out.println("Response body :"+con.getResponseCode());
+						
 			while((line= br.readLine())!=null){
 					strBuilder.append(line);
 				}
 			response_txt = strBuilder.toString();
-			br.close();
-			//System.out.println("response_txt: \n"+response_txt);
-			PrettyPrinter.prettyFormat(response_txt);
-			
+
+			if(error){
+			System.out.println("networking: Response_txt: "+TextFiles.prettyPrintXMLString2Console(response_txt, "", 2));
+			}
+			br.close();			
+	
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}finally{
-			System.out.println("this is finally in sendPOST2Webservice:");
+			//System.out.println("this is finally in sendPOST2Webservice:");
 			if(writer!=null){
 				try{
 					writer.close();
@@ -68,14 +74,8 @@ public void sendPOST2Webservice(String urlService, String msg){
 					con.disconnect();
 				}catch(Exception ex){
 				}
-			}
-			if (bufferedReader != null) {
-        		try {
-                		bufferedReader.close();
-            		} catch (IOException ex) {
-                		throw ex;
-            		}
-        }
+			}	
 		}
+		return response_txt;
 	}
 -----------------------------------
