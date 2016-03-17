0f00ea6877f1671370c2c6d6fcb0ec64ce563556
geronimoooooooo <geronimoooooooo@gmail.com>
2016-03-17 11:24:52 +0100

    Create pathsWithPageContextJSP.java

---
+++ paths/pathsWithPageContextJSP.java
@@ -0,0 +1,12 @@
+${pageContext.request.requestURL} -- http://localhost:8080/geo-websocket/start.jsp 
+${pageContext.request.contextPath} -- /geo-websocket 
+${pageContext.request.serverName} -- localhost 
+${pageContext.request.localPort} -- 8080 
+${pageContext.request.scheme} -- http 
+${pageContext.request.requestURI} -- /geo-websocket/start.jsp 
+
+
+
+
+
+
