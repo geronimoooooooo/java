3c779519d262597e3a28d8b8e69120ce45c9f03d
geronimoooooooo <geronimoooooooo@gmail.com>
2021-09-23 15:14:56 +0200

    Update Streams.java

--- Streams.java
+++ Streams.java
@@ -1,12 +1,13 @@
 Hier findet man alle möglichen Stream Examples:
 https://docs.oracle.com/javase/tutorial/essential/io/bytestreams.html
 ---------------------------------------------------------------------------
+https://www.programiz.com/java-programming/printstream
 Streams... sequence of data
 byte stream (base abstract class InputStream and OutputStream)
     or 
 character stream (base abstract class Reader and Writer)
-
-
+FileInputStream read data(bytes) from a file
+---------------------------------------------------------------------------
 
 InputStream is = urlc.getInputStream();
     int c;
