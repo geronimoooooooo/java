0347364aa723070d47c08f178ca888769e16e625
geronimoooooooo <geronimoooooooo@gmail.com>
2021-10-22 00:24:05 +0200

    Create jsp_links.java

---
+++ jsp/jsp_links.java
@@ -0,0 +1,6 @@
+https://www.w3schools.in/category/jsp/
+https://www.guru99.com/jsp-tutorial.html
+https://www.javatpoint.com/jsp-tutorial
+https://www.w3adda.com/jsp-tutorial/jsp-scriptlettag
+https://www.tutorialspoint.com/jsp/jsp_auto_refresh.htm
+https://www.torsten-horn.de/techdocs/jsp-grundlagen.htm
