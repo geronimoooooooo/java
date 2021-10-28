07b19d8fc5a98c1453ff64785e32b07fc7c27042
geronimoooooooo <geronimoooooooo@gmail.com>
2021-10-28 04:25:12 +0200

    Update AAA_jsp_links.java

--- jsp/AAA_jsp_links.java
+++ jsp/AAA_jsp_links.java
@@ -4,3 +4,12 @@ https://www.javatpoint.com/jsp-tutorial
 https://www.w3adda.com/jsp-tutorial/jsp-scriptlettag
 https://www.tutorialspoint.com/jsp/jsp_auto_refresh.htm
 https://www.torsten-horn.de/techdocs/jsp-grundlagen.htm
+
+https://www.edureka.co/blog/servlet-and-jsp-tutorial/
+https://www.deshbandhucollege.ac.in/pdf/e-resources/computer-science/Bsc(P)-VI-InternetTechnologies-1.pdf
+https://ssw.jku.at/Teaching/Lectures/Sem/2000/Leitenmueller/
+
+https://www.geeksforgeeks.org/difference-between-servlet-and-jsp/
+https://kodejava.org/how-do-i-share-object-or-data-between-users-in-web-application/
+
+https://www.tutorialrepublic.com/jquery-examples.php
