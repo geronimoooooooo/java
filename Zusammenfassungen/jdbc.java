be1eb00e882b08629ebd539a1c72d664f77a59ad
geronimoooooooo <geronimoooooooo@gmail.com>
2022-03-09 16:54:12 +0100

    Update jdbc.java

--- Zusammenfassungen/jdbc.java
+++ Zusammenfassungen/jdbc.java
@@ -6,4 +6,23 @@ https://www.edureka.co/blog/connect-mysql-database-in-java
 
 https://www.edureka.co/blog/advanced-java-tutorial
 + Auflistung der JDBC Driver Names
-+  Web container == Servlet Container
++ Web container == Servlet Container
++ expression tag <%=  statement %> == out.print()  written to the output stream
+
+https://www.edureka.co/blog/servlet-and-jsp-tutorial/
++ How to Build Web Applications in Java?
++ Wie man Cookies setzt
+
+https://www.edureka.co/blog/java-servlets
++ Servlets in a Nutshell
++ classes and Interfaces of a servlet
+++ ServletRequest	Used to read data from the client request
+++ ServletResponse	Used to write data to clients response
+
+https://www.edureka.co/blog/jsp-in-java/
++ Know All About Java Web Applications
+
+https://www.edureka.co/blog/mvc-architecture-in-java/
++ Model – Represents the business layer of the application
++ View – Defines the presentation of the application
++ Controller – Manages the flow of the application
