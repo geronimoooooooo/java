df07d198e3fec4c7ef9a897717ac239d1e8a2aba
geronimoooooooo <geronimoooooooo@gmail.com>
2022-03-09 15:02:42 +0100

    Update jdbc.java

--- Zusammenfassungen/jdbc.java
+++ Zusammenfassungen/jdbc.java
@@ -1,3 +1,9 @@
 https://www.edureka.co/blog/connect-mysql-database-in-java
 + JDBC Java Database Connectivity
++ Java mit DB verbinden
 + Examples für Connection, SELECT, INSERT
+
+
+https://www.edureka.co/blog/advanced-java-tutorial
++ Auflistung der JDBC Driver Names
++  Web container == Servlet Container
