3f05d2cd8bf096ef890bb2fa491b2b36a86e58ed
geronimoooooooo <geronimoooooooo@gmail.com>
2022-03-14 14:17:12 +0100

    Update random.java

--- Zusammenfassungen/random.java
+++ Zusammenfassungen/random.java
@@ -2,5 +2,5 @@
 + Listener statt init() von load-on-startup Servlet
 + Instant time;
 + PrintWriter out = response.getWriter(); out.println("website");
-+ response.getWriter().append("Served at: ").append(request.getContextPath());
-+ response.getWriter().append("Served at: ").append(request.getRequestURL());
++ response.getWriter().append("Served at: ").append(request.getContextPath()); /projekt
++ response.getWriter().append("Served at: ").append(request.getRequestURL()); http://localhost:8080/projekt/servlet
