9628341f0f5f423e337fa200b12e476c95f60a43
geronimoooooooo <geronimoooooooo@gmail.com>
2022-04-04 21:35:52 +0200

    Create tasks.java

---
+++ jdbc/tasks.java
@@ -0,0 +1,5 @@
+-------------------------------------------------------
+Sample Template how to connect, query, and extract from resultSet
+https://www.tutorialspoint.com/jdbc/jdbc-sample-code.htm
+-------------------------------------------------------
+
