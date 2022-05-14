f8ff88c40bb36c46de62d7f1bf71cc7318e3c106
geronimoooooooo <geronimoooooooo@gmail.com>
2022-05-14 22:58:51 +0200

    Create readFileWithinServlet.java

---
+++ readWrite/readFileWithinServlet.java
@@ -0,0 +1,8 @@
+https://www.programiz.com/java-programming/bufferedwriter
+https://kodejava.org/how-do-i-read-text-file-in-servlet/
+How To Use A Java Servlet To Read From A Text File: https://www.dreamincode.net/forums/topic/405733-how-to-use-a-java-servlet-to-read-from-a-text-file/
+How do I read and output a text file from a Servlet? https://www.jguru.com/how-do-i-read-and-output-a-text-file-from-a-servlet/ 
+WRITING TO A FILE FROM A SERVLET IN A JAVA EE WEB APPLICATION https://javajee.com/writing-to-a-file-from-a-servlet-in-a-java-ee-web-application 
+Append to a file in java using BufferedWriter, PrintWriter, FileWriter https://beginnersbook.com/2014/01/how-to-append-to-a-file-in-java/
+Java BufferedReader: How to Read File in Java with Example https://www.guru99.com/buffered-reader-in-java.html
+Java – How to read last few lines of a File https://mkyong.com/java/java-how-to-read-last-few-lines-of-a-file/
