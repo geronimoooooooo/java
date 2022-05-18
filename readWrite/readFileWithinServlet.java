0620f250f17fa2176dc541f9011387e8f265f4b0
geronimoooooooo <geronimoooooooo@gmail.com>
2022-05-18 16:24:30 +0200

    Update readFileWithinServlet.java

--- readWrite/readFileWithinServlet.java
+++ readWrite/readFileWithinServlet.java
@@ -1,8 +1,27 @@
-https://www.programiz.com/java-programming/bufferedwriter
-https://kodejava.org/how-do-i-read-text-file-in-servlet/
-How To Use A Java Servlet To Read From A Text File: https://www.dreamincode.net/forums/topic/405733-how-to-use-a-java-servlet-to-read-from-a-text-file/
-How do I read and output a text file from a Servlet? https://www.jguru.com/how-do-i-read-and-output-a-text-file-from-a-servlet/ 
-WRITING TO A FILE FROM A SERVLET IN A JAVA EE WEB APPLICATION https://javajee.com/writing-to-a-file-from-a-servlet-in-a-java-ee-web-application 
-Append to a file in java using BufferedWriter, PrintWriter, FileWriter https://beginnersbook.com/2014/01/how-to-append-to-a-file-in-java/
-Java BufferedReader: How to Read File in Java with Example https://www.guru99.com/buffered-reader-in-java.html
-Java – How to read last few lines of a File https://mkyong.com/java/java-how-to-read-last-few-lines-of-a-file/
++ https://www.programiz.com/java-programming/bufferedwriter
++ https://kodejava.org/how-do-i-read-text-file-in-servlet/
++ How To Use A Java Servlet To Read From A Text File: https://www.dreamincode.net/forums/topic/405733-how-to-use-a-java-servlet-to-read-from-a-text-file/
++ How do I read and output a text file from a Servlet? https://www.jguru.com/how-do-i-read-and-output-a-text-file-from-a-servlet/ 
++ WRITING TO A FILE FROM A SERVLET IN A JAVA EE WEB APPLICATION https://javajee.com/writing-to-a-file-from-a-servlet-in-a-java-ee-web-application 
++ Append to a file in java using BufferedWriter, PrintWriter, FileWriter https://beginnersbook.com/2014/01/how-to-append-to-a-file-in-java/
++ Java BufferedReader: How to Read File in Java with Example https://www.guru99.com/buffered-reader-in-java.html
++ Java – How to read last few lines of a File https://mkyong.com/java/java-how-to-read-last-few-lines-of-a-file/
+
+
++ https://stackoverflow.com/questions/36250571/filewriter-vs-fileoutputstream-in-java
++ FileOutputStream is meant for writing streams of raw bytes such as image data. For writing streams of characters, consider using FileWriter. If you are familiar with design patterns, FileWriter is a typical usage of Decorator pattern actually.
++ https://stackoverflow.com/questions/8544771/how-to-write-data-with-fileoutputstream-without-losing-old-data
++ https://www.tabnine.com/code/java/methods/java.io.OutputStreamWriter/append
++ https://localcoder.org/delete-last-line-in-text-file
++ https://quick-adviser.com/how-do-you-remove-the-last-line-of-a-file-in-java/
++ Java – How to read last few lines of a File https://mkyong.com/java/java-how-to-read-last-few-lines-of-a-file/
+
+http://www.java2s.com/Tutorial/Java/0400__Servlet/ReadFileinaServlet.htm
+https://www.roseindia.net/servlets/servlet-read-file.shtml
+https://kodejava.org/how-do-i-read-text-file-in-servlet/#:~:text=Using%20the%20ServletContext.,directory%20in%20our%20web%20application.
+https://howtodoinjava.com/java/io/java-append-to-file/
+https://stackoverflow.com/questions/7339342/copy-a-string-to-the-beginning-of-a-file-in-java
+https://www.journaldev.com/881/java-append-to-file
+https://stackoverflow.com/questions/1625234/how-to-append-text-to-an-existing-file-in-java?rq=1
+
+//www.plantuml.com/plantuml/png/ZLDDRzim3BtdL_Yu7NRPQV1XgB70Gx5cosftxMrGRCOHge-Cejhrtu-qrQKEX2GdI-HxZ-GpTHL88KLh5eqSYW-VYpMWzhtkna1PzFuhqBltYrbOS4stWpuQm5Q5nx8OXL9Cku4PCq26WT7bK3yZuCjIaMe0dm3Buey4faljKJAB8upG7gnPAmnG5dlvyFMRqLPzyMwhXuYAj9Tohp2XYC162MK0kb61Lg12HB3WY7F0rHfw9IOm4sV5UiRDH1E33Sbg_jXM3UMQQU4lrTf0u04GY5xMg1qzh6uQuiFmaGS9tW2pJWylh2bnxROURQfr0IlrDNFihCPeShNV__Vm5lX_XN7MBEDWK8V0e8I_GpEpEvsE7VtbNQijL64KvkwLVGlEhKz4CwrGhfdlGCyU7zbWaNVzjEk7Q5wI_qfnsenmeHb7eRIyjz21Ve9U4ZLwEoKp2lEzGL3zFMe2Re5dQRt4xpNj16s0GXKQOf3zVTVMdrZkz59OHTsEFM1EwrVfijmmu4mVpz93Mbi9IRNSIp-yF-q285qxyiiD8w-u-sZDFm00
