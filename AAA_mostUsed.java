82a27a5d35963c57fc5eca36dc9bb9bd9ec85c68
geronimoooooooo <geronimoooooooo@gmail.com>
2022-11-14 17:19:28 +0100

    Update AAA_mostUsed.java

--- AAA_mostUsed.java
+++ AAA_mostUsed.java
@@ -1,2 +1,4 @@
-		PrintWriter out = response.getWriter();
-		out.println("text");
+PrintWriter out = response.getWriter();
+out.println("text");
+
+<a href=${pageContext.request.contextPath}/periodic-start class="btn btn-success">Service Start</a>  	
